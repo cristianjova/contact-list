65c8dbee1d309ff5e8a3839b030db7d11bc8fbdf
Cristian Jovanovich <cristian.jova149@gmail.com>
2019-08-19 20:56:44 -0300

    cleanup project, created navbar component and router setup

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1>About this App</h1>
+      <p className='my-1'>This is full stack React app for keeping contacts</p>
+      <p className='bg-dark p'>
+        <strong>Version: 1.0.0</strong>
+      </p>
+    </div>
+  );
+};
+
+export default About;
