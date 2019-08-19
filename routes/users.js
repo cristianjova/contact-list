1c76baae1f209da5c03b460a000ea010db7448ff
Cristian Jovanovich <cristian.jova149@gmail.com>
2019-08-19 14:59:43 -0300

    hash password & register route

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 const { check, validationResult } = require('express-validator');
 
 const User = require('../models/User');
@@ -19,12 +20,40 @@ router.post(
       'Please enter a password with 6 or more characters'
     ).isLength({ min: 6 })
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    res.send('Passed');
+
+    const { name, email, password } = req.body;
+
+    try {
+      // Second email is from req.body
+      let user = await User.findOne({ email: email });
+
+      if (user) {
+        return res.status(400).json({ msg: 'El usuario ya existe' });
+      }
+
+      user = new User({
+        // This is equal to name: name, email: email
+        name,
+        email,
+        password
+      });
+
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      res.send('User saved');
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
   }
 );
 
