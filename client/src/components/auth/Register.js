604dc9b297645184a22c9c50e271cbb523cb752e
Cristian Jovanovich <cristian.jova149@gmail.com>
2019-08-25 03:16:40 -0300

    created action to user registration

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,9 +1,21 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import AlertContext from '../../context/alert/alertContext';
+import AuthContext from '../../context/auth/authContext';
 
 const Register = () => {
   const alertContext = useContext(AlertContext);
+  const authContext = useContext(AuthContext);
+
   const { setAlert } = alertContext;
+
+  const { register, error, clearErrors } = authContext;
+
+  useEffect(() => {
+    if (error === 'El usuario ya existe') {
+      setAlert(error, 'danger');
+      clearErrors();
+    }
+  }, [error]);
 
   const [user, setUser] = useState({
     name: '',
@@ -23,7 +35,11 @@ const Register = () => {
     } else if (password !== password2) {
       setAlert('Las contraseñas no coinciden', 'danger');
     } else {
-      console.log('Register Submit');
+      register({
+        name,
+        email,
+        password
+      });
     }
   };
 
