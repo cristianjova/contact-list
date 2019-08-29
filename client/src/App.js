3490d4ebc1cc18ba8448c35f6b2625829e82a4e8
Cristian Jovanovich <cristian.jova149@gmail.com>
2019-08-28 21:00:44 -0300

    created actions to loadUser & setToken

--- client/src/App.js
+++ client/src/App.js
@@ -10,8 +10,14 @@ import Alerts from './components/layout/Alerts';
 import ContactState from './context/contact/ContactState';
 import AuthState from './context/auth/AuthState';
 import AlertState from './context/alert/AlertState';
+import setAuthToken from './utils/setAuthToken';
 
 import './App.css';
+
+// Load token into defaults headers
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
 
 const App = () => {
   return (
